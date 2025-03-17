0ef5e66bddfa3346e8f0b3b3a87589a24f485358
Niko Finlertet <nikofinlertet@gmail.com>
2025-03-18 00:03:43 +0300

    Go

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,24 +1,18 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
 
 local lspconfig = require "lspconfig"
+local servers = {"html", "cssls", "gopls"}
 
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
   }
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
